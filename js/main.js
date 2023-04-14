9e4548ba45935f12e184a0fdb8aef78a477c2e43
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2023-04-15 02:23:23 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ function shrink() {
     navbar.classList.remove("navbar-shrink");
   }
 }
+
+
+
+
+var i = 0;
+var titles = [  "Diyorbek | Planning",  "Jo'rabek | Work",  "Nodirbek | About",  "Abdulaziz | Blog", "Behruzbek | Pricing"];
+
+function changeTitle() {
+  document.title = titles[i];
+  i = (i + 1) % titles.length;
+  setTimeout(changeTitle, 750); 
+}
+
+changeTitle();
