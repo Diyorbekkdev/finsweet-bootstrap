e64b7450da7c1656ab254e1e470f051f1b1d2890
7777777lv <jorabekmuhammadov7@gmail.com>
2023-04-14 15:55:04 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
